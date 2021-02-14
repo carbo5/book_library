02525e806d04ab4efbb4d07688b2e220cf2c7282
Bruno Carbonneau <accesshockeystats@gmail.com>
2021-02-14 12:21:50 -0500

    Adding javascript to add and remove a book

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let myLibrary = [];
 
 class Book{
   // the constructor...
-  constructor(isbn, title, author, read,pages){
+  constructor(isbn, title, author, read, pages){
     this.isbn = isbn;
     this.title = title;
     this.author = author;
@@ -11,18 +11,76 @@ class Book{
   }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(e) {
   // do stuff here
+  e.preventDefault();
+
+  const isbn = document.querySelector("#isbn").value;
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").checked;
+
+  console.log(read);
+
+  const book = new Book(isbn, title, author, read, pages);
+
+
+  myLibrary.push(book);
+
+  addBookToTable(book);
+
+  console.log(myLibrary);
+
+
 }
 
-document.querySelector("#book_form").addEventListener("submit",(e) =>{
-    e.preventDefault();
+function addBookToTable(book){
 
-    const isbn = document.querySelector("#isbn").value;
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const pages = document.querySelector("#pages").value;
-    const read = document.querySelector("#read").value;
+  const table_Tbody = document.querySelector('#tableBookBody');
+
+  const row = document.createElement('tr');
+
+  //console.log(`${book.isbn}`);
+
+  let isRead = `<input type="checkbox"></input>`;
+
+  //console.log(book.read);
+
+  if(book.read){
+    isRead = `<input type="checkbox" checked></input>`;
+  }
 
 
+  row.innerHTML = `
+    <td>${book.isbn}</td>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${isRead}</td>
+    <td>${book.pages}</td>
+    <td><a href='#' class="delete">Delete</td>
+  `;
+
+ 
+
+  table_Tbody.appendChild(row);
+
+  /*console.log(row);
+  console.log(table_Tbody);*/
+
+
+}
+
+document.querySelector("#form_book").addEventListener("submit", addBookToLibrary);
+
+document.querySelector('#tableBookBody').addEventListener('click', (e) =>{
+  console.log(e.target);
+  deleteBook(e.target);
 });
+
+function deleteBook(element){
+  if(element.classList.contains('delete')){
+    element.parentElement.parentElement.remove();
+  }
+  
+}
